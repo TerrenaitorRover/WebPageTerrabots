f60b1e2a24d592c20fa6aa11fcc8b465f70568a6
CarlosFGarayH <carlosfgarayh@gmail.com>
2025-10-02 00:04:48 -0500

    Add link WhatsApp Viviana, Remove member Team and Add News Andina

--- src/app/components/body/about/about.component.ts
+++ src/app/components/body/about/about.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  goToLink() {
+    window.open('https://andina.pe/agencia/noticia-estudiantes-de-uni-usan-ia-para-control-rover-planetario-y-destacan-concurso-1046194.aspx', '_blank');
+  }
 }
