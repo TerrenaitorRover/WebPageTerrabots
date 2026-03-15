b1afaba95cbb2ea325d8d2dcc8f0ea6a25886ad7
PatrickFab21 <patrick.echevarria.d@uni.pe>
2026-03-14 20:16:54 -0500

    Add email.service.ts with ContactPayload interface and Netlify function endpoint

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -2,18 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, timeout } from 'rxjs';
 
+export interface ContactPayload {
+  email: string;
+  subject: string;
+  message: string;
+  website: string;
+  startedAt: number;
+  turnstileToken: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  private apiUrl = 'https://servewebterrabots.onrender.com/send-email';
+  private apiUrl = '/.netlify/functions/contact';
 
   constructor(private http: HttpClient) {}
 
-  sendEmail(email: string, subject: string, message: string): Observable<any> {
-    return this.http.post(this.apiUrl, { email, subject, message }).pipe(
-      timeout(30000)
+  sendEmail(payload: ContactPayload): Observable<any> {
+    return this.http.post(this.apiUrl, payload).pipe(
+      timeout(20000)
     );
   }
 }
